5b89dff3b36f4da713f1bad06f79b61c548c1f31
Adam Mika <amika@squareup.com>
2024-02-05 12:27:15 -0700

    Add a `getSigner` function to `BearerDID`

--- Tests/Web5Tests/Dids/BearerDIDTests.swift
+++ Tests/Web5Tests/Dids/BearerDIDTests.swift
@@ -5,13 +5,25 @@ import XCTest
 
 final class BearerDIDTests: XCTestCase {
 
-    func test_export() async throws {
+    func test_export() throws {
         let didJWK = try DIDJWK.create(keyManager: InMemoryKeyManager())
-        let portableDID = try await didJWK.export()
+        let portableDID = try didJWK.export()
 
         XCTAssertNoDifference(portableDID.uri, didJWK.uri)
         XCTAssertNoDifference(portableDID.document, didJWK.document)
         XCTAssertNoDifference(portableDID.privateKeys.count, 1)
         XCTAssertNil(portableDID.metadata)
+    }
+
+    func test_signAndVerify() throws {
+        let payload = "Hello, world!".data(using: .utf8)!
+
+        let didJWK = try DIDJWK.create(keyManager: InMemoryKeyManager())
+        let signer = try didJWK.getSigner()
+
+        let signature = try signer.sign(payload: payload)
+        let isValid = try signer.verify(payload: payload, signature: signature)
+
+        XCTAssertTrue(isValid)
     }
 }
