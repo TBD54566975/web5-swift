6105c8cb6b2f472b549266687a430b431dc8ba1f
Adam Mika <amika@squareup.com>
2024-02-09 08:40:55 -0700

    Convert did:ion resolver to new protocol

--- Sources/Web5/Dids/Methods/DIDIon.swift
+++ Sources/Web5/Dids/Methods/DIDIon.swift
@@ -1,36 +1,77 @@
 import Foundation
 
 /// `did:ion` DID Method
-public enum DIDIon {
+public enum DIDIon {}
 
-    public static let methodName = "ion"
+// MARK: - Resolver
 
-    /// Resolves a `did:ion` URI into a `DIDResolutionResult`
-    /// - Parameters:
-    ///   - didURI: The DID URI to resolve
-    /// - Returns: `DIDResolutionResult` containing the resolved DID Document.
-    public static func resolve(
-        didURI: String
-    ) async -> DIDResolutionResult {
-        guard let did = try? DID(didURI: didURI) else {
-            return DIDResolutionResult(error: .invalidDID)
+extension DIDIon {
+    
+    /// Resolver for the `did:ion` DID method
+    public struct Resolver: DIDMethodResolver {
+
+        /// Options that can be configured for resolving `did:ion` DIDs
+        public struct ResolutionOptions {
+
+            /// The URI of a server involved in executing DID method operations. In the context of
+            /// DID creation, the endpoint is expected to be a Sidetree node.
+            public let gatewayURI: String
+
+            /// Public Memberwise Initializer
+            public init(
+                gatewayURI: String
+            ) {
+                self.gatewayURI = gatewayURI
+            }
+
+            /// Default ResolutionOptions
+            public static let `default` = ResolutionOptions(
+                gatewayURI: "https://ion.tbddev.org"
+            )
         }
 
-        guard did.methodName == Self.methodName else {
-            return DIDResolutionResult(error: .methodNotSupported)
+        /// Initialize a new `DIDIon.Resolver`
+        /// - Parameters:
+        ///   - options: The options to use for resolution process
+        public init(
+            options: ResolutionOptions = .default
+        ) {
+            self.options = options
         }
 
-        let identifiersEndpoint = "https://ion.tbddev.org/identifiers"
-        guard let url = URL(string: "\(identifiersEndpoint)/\(did.uri)") else {
-            return DIDResolutionResult(error: .notFound)
-        }
+        /// The options to use for resolution process
+        public let options: ResolutionOptions
 
-        do {
-            let response = try await URLSession.shared.data(from: url)
-            let resolutionResult = try JSONDecoder().decode(DIDResolutionResult.self, from: response.0)
-            return resolutionResult
-        } catch {
-            return DIDResolutionResult(error: .notFound)
+        // MARK: DIDMethodResolver
+
+        public let methodName = "ion"
+
+        /// Resolves a `did:ion` URI into a `DIDResolutionResult`
+        /// - Parameters:
+        ///   - didURI: The DID URI to resolve
+        /// - Returns: `DIDResolutionResult` containing the resolved DID Document.
+        public func resolve(
+            didURI: String
+        ) async -> DIDResolutionResult {
+            guard let did = try? DID(didURI: didURI) else {
+                return DIDResolutionResult(error: .invalidDID)
+            }
+
+            guard did.methodName == methodName else {
+                return DIDResolutionResult(error: .methodNotSupported)
+            }
+
+            guard let url = URL(string: "\(options.gatewayURI)/identifiers/\(did.uri)") else {
+                return DIDResolutionResult(error: .notFound)
+            }
+
+            do {
+                let response = try await URLSession.shared.data(from: url)
+                let resolutionResult = try JSONDecoder().decode(DIDResolutionResult.self, from: response.0)
+                return resolutionResult
+            } catch {
+                return DIDResolutionResult(error: .notFound)
+            }
         }
     }
 }
--- Sources/Web5/Dids/Methods/Protocols/DIDMethodResolver.swift
+++ Sources/Web5/Dids/Methods/Protocols/DIDMethodResolver.swift
@@ -3,6 +3,11 @@ import Foundation
 /// Protocol defining the behaviors common to all DID method resolvers
 public protocol DIDMethodResolver {
 
+    /// The name of the DID method that this resolver supports
+    ///
+    /// Example: In the DID URI `did:example:123`, the method name is `example`
+    var methodName: String { get }
+
     /// Resolves a DID URI into a `DIDResolutionResult`
     /// - Parameters:
     ///   - didURI: The DID URI to resolve
