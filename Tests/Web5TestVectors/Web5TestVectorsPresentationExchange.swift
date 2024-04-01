40bc321948d69bfb590d4e86ed8f1dcf34b4de21
Kirah Sapong <kirah@tbd.email>
2024-04-01 15:05:19 -0700

    add vector test

--- Tests/Web5TestVectors/Web5TestVectorsPresentationExchange.swift
+++ Tests/Web5TestVectors/Web5TestVectorsPresentationExchange.swift
@@ -6,7 +6,7 @@ import XCTest
 
 final class Web5TestVectorsPresentationExchange: XCTestCase {
 
-    func test_resolve() throws {
+    func test_selectCredentials() throws {
         struct Input: Codable {
             let presentationDefinition: PresentationDefinitionV2
             let credentialJwts: [String]
@@ -46,6 +46,57 @@ final class Web5TestVectorsPresentationExchange: XCTestCase {
                 /// Select valid credentials from each of the inputs, make sure it matches output
                 let result = try PresentationExchange.selectCredentials(vcJWTs: vector.input.credentialJwts, presentationDefinition: vector.input.presentationDefinition)
                 XCTAssertEqual(result.sorted(), vector.output!.selectedCredentials.sorted())
+                expectation.fulfill()
+            }
+
+            wait(for: [expectation], timeout: 1)
+        }
+    }
+    
+    func test_createPresentationFromDefinition() throws {
+        struct Input: Codable {
+            let presentationDefinition: PresentationDefinitionV2
+            let credentialJwts: [String]
+            let mockServer: [String: [String: String]]?
+
+            func mocks() throws -> [Mock] {
+                guard let mockServer = mockServer else { return [] }
+
+                return try mockServer.map({ key, value in
+                    return Mock(
+                        url: URL(string: key)!,
+                        contentType: .json,
+                        statusCode: 200,
+                        data: [
+                            .get: try JSONEncoder().encode(value)
+                        ]
+                    )
+                })
+            }
+        }
+        
+        struct Output: Codable {
+            let presentationSubmission: PresentationSubmission
+        }
+
+        let testVector = try TestVector<Input, Output>(
+            fileName: "create_presentation_from_credentials",
+            subdirectory: "test-vectors/presentation_exchange"
+        )
+
+        testVector.run { vector in
+            let expectation = XCTestExpectation(description: "async resolve")
+            Task {
+                /// Register each of the mock network responses
+                try vector.input.mocks().forEach { $0.register() }
+
+                /// Select valid credentials from each of the inputs
+                let credentials = try PresentationExchange.selectCredentials(vcJWTs: vector.input.credentialJwts, presentationDefinition: vector.input.presentationDefinition)
+                
+                /// Create a presentation submission from the selected credentials and make sure it matches the output
+                let result = try PresentationExchange.createPresentationFromCredentials(vcJWTs: credentials, presentationDefinition: vector.input.presentationDefinition)
+                XCTAssertEqual(result.definitionID, vector.output!.presentationSubmission.definitionID)
+                XCTAssertEqual(result.descriptorMap, vector.output!.presentationSubmission.descriptorMap)
                 expectation.fulfill()
             }
 
