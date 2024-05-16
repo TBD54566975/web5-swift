76cd4b532a49f2055860f766309546a9a186fba4
Alex Hsieh <alex.kyhsieh@gmail.com>
2024-05-16 15:24:44 +0800

    adds JWT tests and makes JWT verify correct

--- Tests/Web5Tests/Crypto/JOSE/JWTTests.swift
+++ Tests/Web5Tests/Crypto/JOSE/JWTTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Foundation
 
 @testable import Web5
 
@@ -45,9 +46,108 @@ final class JWTTests: XCTestCase {
             XCTAssertEqual(error, JWT.Error.verificationFailed("Expected JWT header to contain kid"))
         }
 
+        // decode payload fails
         let header = JWS.Header(algorithm: .es256k, keyID: "keyID", type: "JWT")
         let base64UrlEncodedHeader = try JSONEncoder().encode(header).base64UrlEncodedString()
         XCTAssertThrowsError(try JWT.parse(jwtString: "\(base64UrlEncodedHeader).efgh.hijk"))
+    }
+
+    func testVerifyJwtExpired() async throws {
+        let did = try DIDJWK.create(options: .init(algorithm: .secp256k1))
+        let header = JWS.Header(algorithm: .es256k, keyID: did.document.verificationMethod!.first!.id, type: "JWT")
+        let base64UrlEncodedHeader = try JSONEncoder().encode(header).base64UrlEncodedString()
+        let claims = JWT.Claims(expiration: Date(timeIntervalSinceNow: -1))
+        let base64UrlEncodedClaims = try JSONEncoder().encode(claims).base64UrlEncodedString()
+        do {
+             _ = try await JWT.verify(jwt: "\(base64UrlEncodedHeader).\(base64UrlEncodedClaims).hijk")
+        } catch let error as JWT.Error {
+            XCTAssertEqual(error, JWT.Error.verificationFailed("JWT has expired"))
+        }
+    }
+
+    func testVerifyJwtKeyIdNotDereferenceVerificationMethod() async throws {
+        let did = try DIDJWK.create(options: .init(algorithm: .secp256k1))
+        let header = JWS.Header(algorithm: .es256k, keyID: did.uri, type: "JWT")
+        let base64UrlEncodedHeader = try JSONEncoder().encode(header).base64UrlEncodedString()
+        let claims = JWT.Claims(issuer: did.uri, subject: did.uri, issuedAt: Date())
+        let base64UrlEncodedClaims = try JSONEncoder().encode(claims).base64UrlEncodedString()
+        do {
+             _ = try await JWT.verify(jwt: "\(base64UrlEncodedHeader).\(base64UrlEncodedClaims).hijk")
+        } catch let error as JWT.Error {
+            XCTAssertEqual(error, JWT.Error.verificationFailed("Expected kid in JWT header to dereference a DID Document Verification Method"))
+        }
+    }
+
+    func testVerifyPublicKeyAlgorithmIsNotSupported() async throws {
+        let did = try DIDJWK.create(options: .init(algorithm: .secp256k1))
+        let header = JWS.Header(algorithm: .eddsa, keyID: did.document.verificationMethod!.first!.id, type: "JWT")
+        let base64UrlEncodedHeader = try JSONEncoder().encode(header).base64UrlEncodedString()
+        let claims = JWT.Claims(issuer: did.uri, subject: did.uri, issuedAt: Date())
+        let base64UrlEncodedClaims = try JSONEncoder().encode(claims).base64UrlEncodedString()
+        do {
+             _ = try await JWT.verify(jwt: "\(base64UrlEncodedHeader).\(base64UrlEncodedClaims).hijk")
+        } catch let error as JWT.Error {
+            XCTAssertEqual(error, JWT.Error.verificationFailed("Expected alg in JWT header to match DID Document Verification Method alg"))
+        }
+    }
+
+    func testReturnSignerDidIfVerificationSucceeds() async throws {
+        let didStr = """
+        {
+            "document": {
+                "@context": "https://www.w3.org/ns/did/v1",
+                "assertionMethod": ["did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ#0"],
+                "authentication": ["did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ#0"],
+                "capabilityDelegation": ["did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ#0"],
+                "capabilityInvocation": ["did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ#0"],
+                "id": "did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ",
+                "verificationMethod": [
+                {
+                    "controller": "did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ",
+                    "id": "did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ#0",
+                    "publicKeyJwk": {
+                    "alg": "EdDSA",
+                    "crv": "Ed25519",
+                    "kid": "VbyoMtRcC1xELg-bJA0KhpK8S_j-Q7z74CYjOYIkn6k",
+                    "kty": "OKP",
+                    "x": "_hz3P7BGDoRoRSPhZFXNu4Vdtrc9l_J-42TtxGYwC58"
+                    },
+                    "type": "JsonWebKey"
+                }
+                ]
+            },
+            "metadata": {},
+            "privateKeys": [
+                {
+                "alg": "EdDSA",
+                "crv": "Ed25519",
+                "d": "U28yLLtmy_rjPaqaYhj4tnI0A6SzhJO9bbXl5grT0Rs",
+                "kid": "VbyoMtRcC1xELg-bJA0KhpK8S_j-Q7z74CYjOYIkn6k",
+                "kty": "OKP",
+                "x": "_hz3P7BGDoRoRSPhZFXNu4Vdtrc9l_J-42TtxGYwC58"
+                }
+            ],
+            "uri": "did:jwk:eyJ4IjoiX2h6M1A3QkdEb1JvUlNQaFpGWE51NFZkdHJjOWxfSi00MlR0eEdZd0M1OCIsImtpZCI6IlZieW9NdFJjQzF4RUxnLWJKQTBLaHBLOFNfai1RN3o3NENZak9ZSWtuNmsiLCJjcnYiOiJFZDI1NTE5IiwiYWxnIjoiRWREU0EiLCJrdHkiOiJPS1AifQ"
+        }
+        """
+        let portableDid = try JSONDecoder().decode(PortableDID.self, from: didStr.data(using: .utf8)!)
+        let did = try DIDJWK.import(portableDID: portableDid)
+        let header = JWS.Header(algorithm: .eddsa, keyID: did.document.verificationMethod!.first!.id, type: "JWT")
+        let base64UrlEncodedHeader = try JSONEncoder().encode(header).base64UrlEncodedString()
+        let claims = JWT.Claims(issuer: did.uri, subject: did.uri, issuedAt: Date())
+        let base64UrlEncodedClaims = try JSONEncoder().encode(claims).base64UrlEncodedString()
+        let toSign = "\(base64UrlEncodedHeader).\(base64UrlEncodedClaims)"
+        let toSignBytes = [UInt8](toSign.utf8)
+        let privateKeyJwk = portableDid.privateKeys.first!
+        let signatureBytes = try Ed25519.sign(payload: toSignBytes, privateKey: privateKeyJwk)
+        let base64UrlEncodedSignature = signatureBytes.base64UrlEncodedString()
+        let jwt = "\(toSign).\(base64UrlEncodedSignature)"
+        let decoded = try await JWT.verify(jwt: jwt)
+        XCTAssertEqual(header.algorithm, decoded.header.algorithm)
+        XCTAssertEqual(header.keyID, decoded.header.keyID)
+        XCTAssertEqual(claims.issuer, decoded.payload.issuer)
+        XCTAssertEqual(claims.subject, decoded.payload.subject)
+        XCTAssertEqual(claims.issuedAt, decoded.payload.issuedAt)    
     }
 }
 
