a6457e9dc49db0737c44e0ca6805452087a9964c
Ethan Lee <ethanl@tbd.email>
2024-09-10 16:22:27 -0700

    add `VP1BProof`

---
+++ Sources/Web5/Credentials/VP1B/VP1BProof.swift
@@ -0,0 +1,26 @@
+import Foundation
+import SwiftCBOR
+
+struct VP1BProof {
+    let created: Date
+    let method: String
+    let value: String
+
+    static func from(_ cborMap: CBOR) throws -> VP1BProof {
+        return VP1BProof(
+            created: try VP1BUtils.toInstant(cborMap[CBOR.unsignedInt(182)]!),
+            method: try VP1BUtils.toDID(cborMap[CBOR.unsignedInt(194)]!),
+            value: try VP1BUtils.toMultibase(cborMap[CBOR.unsignedInt(192)]!)
+        )
+    }
+
+    func expanded() -> [String: Any] {
+        return [
+            "type": "Ed25519Signature2020",
+            "created": ISO8601DateFormatter().string(from: created),
+            "verificationMethod": method,
+            "proofPurpose": "assertionMethod",
+            "proofValue": value
+        ]
+    }
+}
