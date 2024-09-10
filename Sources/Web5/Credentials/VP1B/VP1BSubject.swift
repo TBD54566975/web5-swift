5e533c30133861d251ba55e75ea9f0866d2d7247
Ethan Lee <ethanl@tbd.email>
2024-09-10 16:22:14 -0700

    add `VP1BSubject`

---
+++ Sources/Web5/Credentials/VP1B/VP1BSubject.swift
@@ -0,0 +1,21 @@
+import Foundation
+import SwiftCBOR
+
+struct VP1BSubject {
+    let concealedIdToken: String
+    let overAge: Int64
+
+    static func from(_ cborMap: CBOR) throws -> VP1BSubject {
+        return VP1BSubject(
+            concealedIdToken: try VP1BUtils.toMultibase(cborMap[CBOR.unsignedInt(138)]!),
+            overAge: try VP1BUtils.toInt(cborMap[CBOR.unsignedInt(148)]!)
+        )
+    }
+
+    func expanded() -> [String: Any] {
+        return [
+            "overAge": overAge,
+            "concealedIdToken": concealedIdToken
+        ]
+    }
+}
