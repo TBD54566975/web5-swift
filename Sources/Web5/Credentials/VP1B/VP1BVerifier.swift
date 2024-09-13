49b0ef1134f7636e34d6ea6464712e50dfdd73b6
Ethan Lee <ethanl@tbd.email>
2024-09-13 11:41:04 -0700

    use `.verify()`

--- Sources/Web5/Credentials/VP1B/VP1BVerifier.swift
+++ Sources/Web5/Credentials/VP1B/VP1BVerifier.swift
@@ -5,27 +5,16 @@ public struct VP1BVerifier {
 
     public static func verifyQRCode(qrcode: String) throws -> VP1B {
         let vp1b = try VP1B.from(qrcode: qrcode)
-
-        let payload = VP1BUtils.generatePayload(from: vp1b)
-        let signature = try VP1BUtils.generateSignature(from: vp1b)
-        let publicKey = try VP1BUtils.generatePublicKey(from: vp1b)
-
-        let isValid = try Ed25519.verify(
-            payload: payload,
-            signature: signature,
-            publicKey: publicKey
-        )
-
-        // TODO: throw error if signature verification fails
-        print("isValid: \(isValid)")
+        try vp1b.verify()
 
         return vp1b
     }
 
-    public static func verifyIssuer(credential: VP1BCredential) async throws -> Bool {
+    public static func verifyIssuer(credential: VP1BCredential) async throws -> VP1BIssuer {
         let issuers = try await VP1BIssuers.fetchIssuers()
-        let issuer = try VP1BIssuers.findIssuer(for: credential, in: issuers)
+        let issuer = try issuers.findIssuer(credential: credential)
+        try issuer.verify()
 
-        return try VP1BIssuers.isValidCredentialType(issuer: issuer);
+        return issuer
     }
 }
